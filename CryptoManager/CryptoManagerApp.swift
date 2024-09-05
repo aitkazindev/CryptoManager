ec07d3583bb3ca50160cea11b31baa65baabcfc7
Ibrahim <aitkazindev@gmail.com>
2024-09-05 17:54:21 +0900

    Initial Commit

---
+++ CryptoManager/CryptoManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoManagerApp.swift
+//  CryptoManager
+//
+//  Created by Ibrahim Aitkazin on 05.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct CryptoManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
