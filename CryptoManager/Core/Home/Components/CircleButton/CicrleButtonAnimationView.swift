dd084349adcefb6c7313112cf3928565a8f3160c
Ibrahim <aitkazindev@gmail.com>
2024-09-05 20:22:57 +0900

    - added custom header for home view - added custom circle button - added custom animation

---
+++ CryptoManager/Core/Home/Components/CircleButton/CicrleButtonAnimationView.swift
@@ -0,0 +1,18 @@
+//
+//  CicrleButtonAnimationView.swift
+//  CryptoManager
+//
+//  Created by Ibrahim Aitkazin on 05.09.2024.
+//
+
+import SwiftUI
+
+struct CicrleButtonAnimationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CicrleButtonAnimationView()
+}
---
+++ CryptoManager/Core/Home/Components/CircleButton/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  CryptoManager
+//
+//  Created by Ibrahim Aitkazin on 05.09.2024.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
